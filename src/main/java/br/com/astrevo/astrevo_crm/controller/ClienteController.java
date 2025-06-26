622d28b7050d6ce38ea74989d42f6207675017d7
Henrique Paulino <hcpaulino-dev@gmail.com>
2025-06-26 17:34:01 -0300

    feat: cria o endpoint cadastrar para cadastro de clientes

--- src/main/java/br/com/astrevo/astrevo_crm/controller/ClienteController.java
+++ src/main/java/br/com/astrevo/astrevo_crm/controller/ClienteController.java
@@ -1,11 +1,27 @@
 package br.com.astrevo.astrevo_crm.controller;
 
+import br.com.astrevo.astrevo_crm.dto.CadastrarClienteDto;
+import br.com.astrevo.astrevo_crm.dto.InformarClienteDto;
+import br.com.astrevo.astrevo_crm.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 
-    public
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostMapping
+    public ResponseEntity<InformarClienteDto> cadastrar(CadastrarClienteDto dto, UriComponentsBuilder uriComponentsBuilder){
+        var cliente = clienteService.cadastrarCliente(dto);
+        var uri = uriComponentsBuilder.path("/clientes/{id}").buildAndExpand(cliente.id()).toUri();
+
+        return ResponseEntity.created(uri).body(cliente);
+    }
 }
--- src/main/java/br/com/astrevo/astrevo_crm/service/ClienteService.java
+++ src/main/java/br/com/astrevo/astrevo_crm/service/ClienteService.java
@@ -1,9 +1,23 @@
 package br.com.astrevo.astrevo_crm.service;
 
+import br.com.astrevo.astrevo_crm.dto.CadastrarClienteDto;
+import br.com.astrevo.astrevo_crm.dto.InformarClienteDto;
+import br.com.astrevo.astrevo_crm.entity.Cliente;
+import br.com.astrevo.astrevo_crm.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService {
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public InformarClienteDto cadastrarCliente(CadastrarClienteDto dto){
+        Cliente cliente = new Cliente(dto);
+        clienteRepository.save(cliente);
+
+        return new InformarClienteDto(cliente);
+    }
 
 }
