35aaa65aba87d2a857a3702888d9d8e3ee3e67d7
Henrique Paulino <hcpaulino-dev@gmail.com>
2025-06-27 11:57:52 -0300

    fest: insere validações para cadastro de cliente

--- src/main/java/br/com/astrevo/astrevo_crm/dto/CadastrarClienteDto.java
+++ src/main/java/br/com/astrevo/astrevo_crm/dto/CadastrarClienteDto.java
@@ -3,16 +3,34 @@ package br.com.astrevo.astrevo_crm.dto;
 import br.com.astrevo.astrevo_crm.entity.Endereco;
 import br.com.astrevo.astrevo_crm.entity.Status;
 import br.com.astrevo.astrevo_crm.entity.TipoPessoa;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CadastrarClienteDto(
-        String id,
+        @NotBlank
         String contato,
+
+        @Email
+        @NotNull
         String email,
+
+        @NotBlank
         String telefone,
+
+        @NotBlank
         String documento,
+
+        @NotNull
         TipoPessoa tipoPessoa,
+
+        @NotBlank
         String nomeEmpresa,
+
+        @NotNull
         Status status,
+
+        @NotNull
         Endereco endereco
 ) {
 }
