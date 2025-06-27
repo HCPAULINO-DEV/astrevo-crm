8fe75e50f3cd6afbb4274e7f4a5c8edcf0f5debf
Henrique Paulino <hcpaulino-dev@gmail.com>
2025-06-27 11:10:51 -0300

    fest: adiciona padrão de paginação no endpoint para buscar clientes

--- src/main/java/br/com/astrevo/astrevo_crm/controller/ClienteController.java
+++ src/main/java/br/com/astrevo/astrevo_crm/controller/ClienteController.java
@@ -6,12 +6,15 @@ import br.com.astrevo.astrevo_crm.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/clientes")
@@ -28,7 +31,7 @@ public class ClienteController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<InformarClienteDto>> buscarClientes(Pageable pageable){
+    public ResponseEntity<Page<InformarClienteDto>> buscarClientes(@PageableDefault(sort = "id", size = 10) Pageable pageable){
         var clientes = clienteService.buscarClientes(pageable);
         return ResponseEntity.ok(clientes);
     }
