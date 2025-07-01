97632559a8adc01d4d1262332da5bb29bbeebfe2
Henrique Paulino <hcpaulino-dev@gmail.com>
2025-07-01 15:43:20 -0300

    feat: automatiza teste de API para buscar cliente pelo id

--- src/test/java/br/com/astrevo/astrevo_crm/api/ClienteTest.java
+++ src/test/java/br/com/astrevo/astrevo_crm/api/ClienteTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.UUID;
+
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.post;
 import static org.hamcrest.Matchers.*;
@@ -78,7 +80,7 @@ public class ClienteTest {
     }
 
     @Test
-    public void deveBuscarClientesCadastrados(){
+    public void deveBuscarClientesCadastrados() {
         //CADASTRAR CLIENTE
         String contato = "user-test";
         String email = "user-test@email.com";
@@ -134,6 +136,69 @@ public class ClienteTest {
                 .body("content[0].nomeEmpresa", equalTo(nomeEmpresa))
                 .body("content[0].status", equalTo(status))
                 .body("content[0].endereco", notNullValue());
+    }
+
+    @Test
+    public void deveBuscarClientePeloId() {
+        //CADASTRAR CLIENTE
+        String contato = "user-test";
+        String email = "user-test@email.com";
+        String telefone = "(11) 91234-5678";
+        String documento = "123.456.789-00";
+        String tipoPessoa = "FISICA";
+        String nomeEmpresa = "Company Test";
+        String status = "ATIVO";
+
+        String json = """
+                    {
+                      "contato": "%s",
+                      "email": "%s",
+                      "telefone": "%s",
+                      "documento": "%s",
+                      "tipoPessoa": "%s",
+                      "nomeEmpresa": "%s",
+                      "status": "%s",
+                      "endereco": {
+                        "cep": "13309-000",
+                        "logradouro": "Rua das Orquídeas",
+                        "numero": "123",
+                        "complemento": "Bloco B, Apto 204",
+                        "bairro": "Jardim Europa",
+                        "cidade": "Itu",
+                        "estado": "SP",
+                        "pais": "Brasil"
+                      }
+                    }
+                """.formatted(contato, email, telefone, documento, tipoPessoa, nomeEmpresa, status);
+
+        String idCliente =
+                given()
+                        .contentType("application/json")
+                        .body(json)
+                        .when()
+                        .post("/clientes")
+                        .then()
+                        .extract()
+                        .path("id");
+
+        //BUSCAR CLIENTE CADASTRADO PELO ID
+        given()
+                .accept("application/json")
+                .log().all()
+                .when()
+                .get("/clientes/" + idCliente)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("contato", equalTo(contato))
+                .body("email", equalTo(email))
+                .body("telefone", equalTo(telefone))
+                .body("documento", equalTo(documento))
+                .body("tipoPessoa", equalTo(tipoPessoa))
+                .body("nomeEmpresa", equalTo(nomeEmpresa))
+                .body("status", equalTo(status))
+                .body("endereco", notNullValue());
+
     }
 
 }
